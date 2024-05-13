3b5908c10decad5e3c3e980f0d3ddccb1adc2987
Bharathi1292 <dibbidibharathi@gmail.com>
2024-05-13 12:00:36 +0200

    creating new files and deleted unused

--- csvtojson.py
+++ csvtojson.py
@@ -2,14 +2,18 @@ import csv
 import json
 
 def csv_to_json(csv_file, json_file):
-    
-    with open(csv_file, 'r') as csv_file:
-        
+    # Open the CSV file with specified encoding
+    with open(csv_file, 'r', encoding='utf-8') as csv_file:
+        # Read the CSV file using csv.DictReader
         csv_reader = csv.DictReader(csv_file)
+        
+        # Convert the CSV data to a list of dictionaries
         data = list(csv_reader)
     
-    with open(json_file, 'w') as json_file:
-        json_file.write(json.dumps(data, indent=4))
+    # Write the data to a JSON file
+    with open(json_file, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 # Example usage
-csv_to_json('profiles1.csv', 'movies.json')
+csv_to_json('profiles1.csv', 'data.json')
+
